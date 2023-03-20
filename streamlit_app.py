7892f67bec66c3b3f64ed4f2e56055ee581f6efc
SiangbaMM <95052185+SiangbaMM@users.noreply.github.com>
2023-03-20 10:45:43 +0100

    Update streamlit_app.py
    Update streamlit_app.py with my certification roadmap

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My first snowflake API')
+streamlit.header("What's next")
+streamlit.text("First of all learn and obtain snowflake certification")
+streamlit.text("After that why not dbt certification")
+streamlit.text("And at the end of this year one more azure certification")
+streamlit.text("This is my certification roadmap for 2023")
+

8804f653344695b64cc2e9250be1a5c1e349fc52
SiangbaMM <95052185+SiangbaMM@users.noreply.github.com>
2023-03-20 11:07:15 +0100

    Update streamlit_app.py
    Update streamlit_app.py by setting dataframe index

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,12 @@ streamlit.text("And at the end of this year one more azure certification as a di
 streamlit.text("This is my certification roadmap for 2023")
 
 streamlit.title("🍌🥭 With those meals, i'll of course build these fruits smoothies below 🥝🍇")
+
 # reading our CSV file from the S3 bucket and pull the data into a dataframe we'll call my_fruit_list. 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# setting dataframe index
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))

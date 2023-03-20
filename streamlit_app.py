3962081bf978d9d01b4d79e19a52ed3ad86a3707
SiangbaMM <95052185+SiangbaMM@users.noreply.github.com>
2023-03-20 16:48:59 +0100

    Update streamlit_app.py
    Dynamic fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,9 @@ streamlit.dataframe(fruits_to_show)
 
 # Calling the Fruityvice API from Our Streamlit App!
 streamlit.header("🥣 🥗 🍞 Fruityvice Fruit Advice 🐔 🥑")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Normalized json file
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Displaying dataframe

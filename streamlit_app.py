b6b83a0e916826973662361dd2d755209cfcbaa8
SiangbaMM <95052185+SiangbaMM@users.noreply.github.com>
2023-03-22 12:50:23 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,16 +31,15 @@ def add_fruit_into_fruit_list(my_cnx, fruit_to_add):
     with my_cnx.cursor() as my_cur:
       query = "INSERT INTO FRUIT_LOAD_LIST(FRUIT_NAME) VALUES('" + fruit_to_add + "')"
       my_cur.execute(query)
-      streamlit.write('Thanks for adding ', fruit_to_add)
+      return 'Thanks for adding ' + fruit_to_add
   except:
     streamlit.error("Qurey issue")
 
+streamlit.title("🍌🥭 With those meals, i'll of course build these fruits smoothies below 🥝🍇")
 # reading our CSV file from the S3 bucket and pull the data into a dataframe we'll call my_fruit_list. 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-
 # setting dataframe index
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
@@ -77,5 +76,9 @@ if streamlit.button("Get trial account metadata"):
   streamlit.dataframe(my_data_row)
 
 # Adding a fruit into the list
-fruit_to_add = streamlit.text_input('What is the fruit you would like to add?','')
-add_fruit_into_fruit_list(fruit_to_add)
+streamlit.header("What fruit would you like to add ?")
+fruit_to_add = streamlit.text_input('Please enter the fruit name :','')
+if streamlit.button("Add the fruit"):
+  my_data_row = add_fruit_into_fruit_list(my_cnx, fruit_to_add)
+  streamlit.text(my_data_row)
+
